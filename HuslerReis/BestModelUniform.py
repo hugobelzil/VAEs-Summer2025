fd2d1094cc5a14e411dd9113c50a56fe275ee80b
Your Name <hugo.belzil@gmail.com>
2025-07-12 17:46:16 -0400

    update exp uniformization

--- HuslerReis/BestModelUniform.py
+++ HuslerReis/BestModelUniform.py
@@ -2,6 +2,7 @@ from Models.LogitNormalVAE import *
 from HuslerReisVAE import uniformity_penalty, custom_loss, dataHR, eval_dataHR
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from scipy.stats import kstest
 
 negative_log_likelihood = lambda x, rv_x: -rv_x.log_prob(x)
 
@@ -76,21 +77,37 @@ if plot_margins:
 # MODEL RE-TRAINING : UNIFORMIZATION OF THE MARGINS
 # In this part below, we retrain the lowest validation loss achieving model
 # with the custom function added to the ELBO in order to enforce the uniformity of the margins
-UNIFORITY_WEIGHT = 0.0001
+UNIFORMITY_WEIGHT = 10 #0.0001
 
 if __name__ == '__main__':
     model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=0.001),
-            loss=custom_loss(uniformity_weight=UNIFORITY_WEIGHT))
+            loss=custom_loss(uniformity_weight=UNIFORMITY_WEIGHT))
 
     model.fit(dataHR, dataHR, validation_data = (eval_dataHR, eval_dataHR),
               batch_size = 32,
-              epochs = 2)
+              epochs = 30)
 
+    print(f"Uniformization model has finished training with uniformity weight = {UNIFORMITY_WEIGHT}")
     post_training_analysis = True
 
     if post_training_analysis:
         prior_samples = model.encoder.prior.sample(N_samples)
         samples_vae = model.decoder(prior_samples).sample()
         plt.scatter(samples_vae[:, 0], samples_vae[:, 1], s=10, marker='o', alpha=0.2)
-        plt.title(f'Simulated data from a HR copula by the VAE({N_samples} samples) AFTER uniformization')
+        plt.title(f'Simulated data from a HR copula by the VAE({N_samples} samples) \n AFTER uniformization')
         plt.show()
+
+        plt.hist(samples_vae[:, 0], bins='auto', edgecolor='white', color='#3B82F6', density=True)
+        plt.axline((0, 1), (1, 1), color='grey', linestyle='--', linewidth=1)
+        plt.title('Histogram of first marginal Husler-Reis Samples')
+        plt.show()
+
+        plt.hist(samples_vae[:, 1], bins='auto', edgecolor='white', color='#3B82F6', density=True)
+        plt.axline((0, 1), (1, 1), color='grey', linestyle='--', linewidth=1)
+        plt.title('Histogram of second marginal Husler-Reis Samples')
+        plt.show()
+
+    statistic1, pvalue1 = kstest(samples_vae[:,0],'uniform', N=2000)
+    statistic2, pvalue2 = kstest(samples_vae[:,1],'uniform', N=2000)
+    print(f"p-value of the KS test for margin 1 : {pvalue1}")
+    print(f"p-value of the KS test for margin 2 : {pvalue2}")
